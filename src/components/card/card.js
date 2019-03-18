cb329e6a14b3ba1a7ed22c2194c3f87d2f54abbb
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-03-18 14:44:09 +0300

    created initial project

---
+++ src/components/card/card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./card.css";
+import Form from "../form/form";
+const Card = () =>{
+    return(
+        <div className="cardWrapper">
+        <h1>card component</h1>
+        <Form/>
+        </div>
+
+    );
+}
+
+export default Card;

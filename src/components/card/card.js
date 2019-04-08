35ae61d7e60af4cc244b4f20553483a1dd36b7ce
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-04-08 12:54:09 +0300

    changed the card into a functional component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,31 +1,13 @@
 import React from "react";
 import "./card.css";
 import Form from '../../containers/form/form';
-//need to set the correct path and change this into a wrapper
-class Card extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            name : "",
-            phoneNumber : "",
-            email : ""
-        }
-    }
 
-    handleSave = (fullName,phoneNumber,email)=>{
-        this.setState({
-            "name" : fullName,
-            "phoneNumber" : phoneNumber,
-            "email" : email
-        });
-    }
-    render(){
+function Card(props){
         return(
             <div className="cardWrapper">
-            <Form handleSave={this.handleSave}/>
+            <Form/>
             </div>
         ); 
-    }
 }
 
 export default Card;

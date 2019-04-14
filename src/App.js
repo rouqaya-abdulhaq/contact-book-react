7cc49153f4db2671c63b50e58fca7fc0aef3c47f
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-04-14 22:20:49 +0300

    added a simple start up logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from "./components/card/card";
+import Logo from './components/logo/logo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Card/>
+        <Logo/>
+        <Logo/>
+      
+        {/*
+         <Regestration/>
+         <ContactsList/>
+         <Footer/>
+         <Palette/>
+        */}
       </div>
     );
   }

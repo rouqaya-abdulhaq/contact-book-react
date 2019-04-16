8b9ca9f878a3f9d223e71a98b7c303771347d1cf
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-04-16 14:49:43 +0300

    changed the regestrtion's folders path

--- src/containers/regestration/regestration.js
+++ src/containers/regestration/regestration.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './regestration.css';
-import Button from '../button/button';
+import Button from '../../components/button/button';
 
 /* should split this into sign in component sign out component and log
 in component and then make this a sperate button component*/ 
 const Regestration = (props)=>{
     return(
         <div>
-          <Button className='navButton' value="sign out"/>
+          <Button className='navButton' value="sign in"/>
+          <Button className='navButton' value="log in"/>
         </div>
     );
 

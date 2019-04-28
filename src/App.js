4ae0cdd356f64504c5cf2e89159012a4865dad15
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-04-28 19:09:21 +0300

    created a plaette component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import './App.css';
 import Header from './containers/header/header';
 import ContactList from './containers/contact-list/contactList';
 import Footer from "./components/footer/footer";
+import Palette from "./components/palette/palette";
 
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      styles : []
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Header/>
         <ContactList/>
         <Footer author={"rouqaya abdulhaq"}/>
-        {/*
          <Palette/>
-        */}
       </div>
     );
   }
---
+++ src/components/palette/palette.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './palette.css';
+
+const Palette = (props)=> {
+    return(
+        <div className="palette">
+           
+        </div>
+    );
+}
+
+export default Palette;

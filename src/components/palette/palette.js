f349ef5386a62021826843841cf267a1470539b9
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-05-02 19:10:00 +0300

    added a palette icon

--- src/components/palette/palette.js
+++ src/components/palette/palette.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './palette.css';
+import PaletteImage from './color_palette.png';
 
 const Palette = (props)=> {
     return(
         <div className="palette">
-           
+           <img src={PaletteImage} alt="palette"></img>
         </div>
     );
 }

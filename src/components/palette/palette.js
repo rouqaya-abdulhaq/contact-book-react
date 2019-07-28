135732279e5e41fb71b262ef5bf4407a751c9042
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-07-28 17:00:47 +0300

    added the style palette pop up.

--- src/components/palette/palette.js
+++ src/components/palette/palette.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './palette.css';
 import PaletteImage from './color_palette.png';
+import PopUp from '../popUp/popUp';
 
-const Palette = (props)=> {
-    return(
-        <div className="palette">
-           <img src={PaletteImage} alt="palette"></img>
-        </div>
-    );
+class Palette extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            stylePopUpDisplay : false,
+        }
+    }
+    
+    displayStylePopUp = () => {
+        this.setState({
+            stylePopUpDisplay : !this.state.stylePopUpDisplay
+        })
+    }
+
+    render(){
+        return(
+            <div className="palette">
+               <img src={PaletteImage} alt="palette" 
+               onClick={this.displayStylePopUp}></img>
+               <PopUp display={this.state.stylePopUpDisplay}>
+                   <p>styles</p>
+               </PopUp>
+            </div>
+        );
+    }
 }
 
 export default Palette;

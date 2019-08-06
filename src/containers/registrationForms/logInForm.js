11e95dfc1621d70539d7c6179da0ec01bb5cde0b
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-08-06 22:34:32 +0300

    created sign/log forms to be worked on in optimization

---
+++ src/containers/registrationForms/logInForm.js
@@ -0,0 +1,22 @@
+import React , {Component} from 'react';
+import Input from '../../components/input/input';
+//will be continued in optimization
+class logInForm extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+                email: "",
+                password: "",
+        }
+    }
+
+    render(){
+        return(
+            <form>
+
+            </form>
+        );
+    }
+}
+
+export default logInForm;

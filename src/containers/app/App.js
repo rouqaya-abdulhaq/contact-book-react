103f74969c24b18dce835f88d645115e50976b41
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-08-18 22:48:29 +0300

    moved app to containers in it's own folder

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
 import './App.css';
-import Header from './components/header/header';
-import ContactList from './containers/contact-list/contactList';
-import Footer from "./components/footer/footer";
-import Palette from "./components/palette/palette";
-import SignUp from './components/signUp/signUp';
-import LogIn from './components/logIn/logIn';
+import Header from '../../components/header/header';
+import ContactList from '../contact-list/contactList';
+import Footer from "../../components/footer/footer";
+import Palette from "../../components/palette/palette";
+import SignUp from '../../components/signUp/signUp';
+import LogIn from '../../components/logIn/logIn';
 
 
 

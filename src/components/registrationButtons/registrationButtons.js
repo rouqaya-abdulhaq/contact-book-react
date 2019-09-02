154096eb2bd54585ddc64ce621b0a1442840ac2f
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-09-02 22:14:47 +0300

    deleted unnecessary css files.

--- src/components/registrationButtons/registrationButtons.js
+++ src/components/registrationButtons/registrationButtons.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import './registrationButtons.css';
 import {Link } from 'react-router-dom';
 
-//the signout button and log sign buttons styles are in header css while the registration
-//styles only the form and it's elements it makes no sense  
 const  Registration = (props) => { 
       if(props.isRegistered === true){
         return (

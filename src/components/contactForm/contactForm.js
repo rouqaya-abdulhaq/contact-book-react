89ce201ea16f1802e0f57f7b6640f740596dc7be
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-02-08 19:23:24 +0300

    default value for contactForm inputs

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -6,17 +6,17 @@ const ContactForm = (props) =>{
     return(
         <div>
             {props.children}
-            <Input label={"first name"} id="firstName" value={props.firstname}
+            <Input label={"first name"} id="firstName" value={props.values.firstName || ''}
               type={"text"} changeHandler={props.changehandler}/>
 
-              <Input label={"last name"} id="lastName" value={props.lastname}
+              <Input label={"last name"} id="lastName" value={props.values.lastName || ''}
               type={"text"} changeHandler={props.changehandler}/>
 
-              <Input label={"email"} id="email" value={props.email}
+              <Input label={"email"} id="email" value={props.values.email || ''}
               type={"email"} changeHandler={props.changehandler}/>
 
               <Input label={"phone number"} id="phoneNumber" type={"tel"} 
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={props.phonenumber}
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={props.values.phoneNumber || ''}
               changeHandler={props.changehandler}/>
 
               <button className="submitButton" onClick={props.submitformhandler}>

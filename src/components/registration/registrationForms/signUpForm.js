c29bf4d176cdd81f88af87a5b1bc50cbb0e4fad0
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-02-18 22:53:06 +0300

    validating signUpForm for correct input form

--- src/components/registration/registrationForms/signUpForm.js
+++ src/components/registration/registrationForms/signUpForm.js
@@ -30,16 +30,28 @@ const signUpForm = (props) =>{
               })
       }
 
+        const validEmail = props.values.email === undefined ? true : props.values.email.isValid;
+        const emailChanged = props.values.email === undefined ? false : props.values.email.hasChanged;
+
+        const validPassword = props.values.password === undefined ? true : props.values.password.isValid;
+        const passwordChanged = props.values.password === undefined ? false : props.values.password.hasChanged;
+
+        const validUser = props.values.user === undefined ? true : props.values.user.isValid;
+        const userChanged = props.values.user === undefined ? false : props.values.user.hasChanged;
+
         return(
           <div>
             <Input label={"user name"} id={"user"} inputType={"name"}
-              type={"text"} changeHandler={props.changehandler}/>
+              type={"text"} changeHandler={props.changehandler}
+              isValid={validUser} hasChanged={userChanged}/>
 
             <Input label={"email"} id="email" inputType={"email"} value={props.email}
-              type ={"email"} changeHandler={props.changehandler}/>
+              type ={"email"} changeHandler={props.changehandler}
+              isValid={validEmail} hasChanged={emailChanged}/>
 
             <Input label={"password"} id="password" inputType={"name"} value={props.password}
-              type={"password"} changeHandler={props.changehandler}/>
+              type={"password"} changeHandler={props.changehandler}
+              isValid={validPassword} hasChanged={passwordChanged}/>
 
                 <button className="submitButton" type="submit" 
                   onClick={()=>fetchUser(props.values.email.value,props.values.password.value,props.values.user.value)}>

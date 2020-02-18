bd20d49cb5cf68cbb3c8e435c5667526f2be55d2
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-02-18 22:47:52 +0300

    validating logInForm for correct input form

--- src/components/registration/registrationForms/logInForm.js
+++ src/components/registration/registrationForms/logInForm.js
@@ -27,14 +27,23 @@ const logInForm = (props) => {
                     //
                 })
         }
+
+        const validEmail = props.values.email === undefined ? true : props.values.email.isValid;
+        const emailChanged = props.values.email === undefined ? false : props.values.email.hasChanged;
+
+        const validPassword = props.values.password === undefined ? true : props.values.password.isValid;
+        const passwordChanged = props.values.password === undefined ? false : props.values.password.hasChanged;
+
         return(
                 <div>
                  <Input label={"email"} id="email" inputType={"email"} value={props.email}
-                     type ={"email"} changeHandler={props.changehandler}/>
+                     type ={"email"} changeHandler={props.changehandler}
+                     isValid={validEmail} hasChanged={emailChanged}/>
 
 
                  <Input label={"password"} id="password" inputType={"name"} value={props.password}
-                    type={"password"} changeHandler={props.changehandler}/>
+                    type={"password"} changeHandler={props.changehandler}
+                    isValid={validPassword} hasChanged={passwordChanged}/>
                     <button className={"submitButton"} 
                       onClick={()=>fetchUser(props.values.email.value,props.values.password.value)} 
                       type="submit">

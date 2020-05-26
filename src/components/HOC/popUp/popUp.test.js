16a31a4e77461aa6b9bc38687a3f26372092ee2f
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-05-26 22:02:38 +0300

    wrote tests for pop up

---
+++ src/components/HOC/popUp/popUp.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import PopUp from './popUp';
+
+describe('<PopUp/>',()=>{
+    it('should have className hide',() =>{
+        const wrapper = shallow(<PopUp />);
+        expect(wrapper.hasClass('hide'));
+    })
+
+    it('should have className show',() =>{
+        const wrapper = shallow(<PopUp />);
+        wrapper.setProps({display : true});
+        expect(wrapper.hasClass('show'));
+    })
+})
